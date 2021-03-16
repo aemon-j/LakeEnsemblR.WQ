c40184568611b4f1f85df981a77e05b9a40b2333
Jorrit Mesman <42146440+jorritmesman@users.noreply.github.com>
2021-03-16 17:04:24 +0100

    set_value_config now works for GOTM-Selmaprotbas

--- R/set_value_config.R
+++ R/set_value_config.R
@@ -2,26 +2,62 @@
 #'
 #'Set value in model-specific config file based on dictionary path
 #'
+#'@param config_file character; path to LakeEnsemblR_WQ config file
 #'@param module character; 
 #'@param process character; 
 #'@param subprocess character; 
-#'@param model character; options c("aed2", "selmaprotbas", "wet", "mylake", "pclake")
-#'@param parameter character; 
+#'@param model_coupled character; options one of "GLM-AED2", "GOTM-Selmaprotbas", "GOTM-WET",
+#'                                       "Simstrat-AED2", "MyLake", "PCLake"
+#'@param parameter character;
+#'@param value character or numeric; what value to enter 
 #'@param dict data.frame; the LakeEnsemblR_WQ dictionary 
+#'@param folder path; relative to what folder 
+#'@param verbose boolean; print output to console
+#'
 #'@examples
+#'
+#'@importFrom configr read.config
+#'@importFrom LakeEnsemblR input_yaml_multiple
 #'
 #'@export
 
 # Note: when we can actually build the package, the "dict"
 #  argument can be removed so that the dictionary in the "data" folder is used
 
-set_value_config <- function(module, process, subprocess, model,
-                          parameter, dict){
+# # Test
+# folder = "."
+# config_file = "LakeEnsemblR_WQ.yaml"
+# module = "oxygen"
+# process = "initial_conditions"
+# subprocess = "initial_water"
+# model_coupled = "GOTM-Selmaprotbas"
+# parameter = "o2"
+# value = 42
+# verbose = TRUE
+
+set_value_config <- function(config_file, module, process, subprocess, model_coupled,
+                          parameter, value, dict, folder, verbose = FALSE){
+  
+  if(model_coupled == "GLM-AED2"){
+    model <- "aed2"
+  }else if(model_coupled == "GOTM-Selmaprotbas"){
+    model <- "selmaprotbas"
+  }else if(model_coupled == "GOTM-WET"){
+    model <- "wet"
+  }else if(model_coupled == "Simstrat-AED2"){
+    model <- "aed2"
+  }else if(model_coupled == "MyLake"){
+    model <- "mylake"
+  }else if(model_coupled == "PCLake"){
+    model <- "pclake"
+  }else{
+    model <- model_coupled
+  }
   
   # Check if arguments are allowed
   chck_args <- sapply(c("module", "process", "subprocess", "model", "parameter"),
                      function(x) get(x) %in% dict[[x]])
-  if(any(isFALSE(chck_args))){
+  if(!all(chck_args)){
     wrong_args <- c("module", "process",
                     "subprocess", "model", "parameter")[!chck_args]
     error_string <- unlist(lapply(wrong_args, function(x) paste0("\n", x, ": ", get(x))))
@@ -40,6 +76,19 @@ set_value_config <- function(module, process, subprocess, model,
     stop("The parameter was not found in the dictionary for this combination of arguments.")
   }
   
+  lst_config <- read.config(file.path(folder, config_file))
   
+  if(model_coupled == "GOTM-Selmaprotbas"){
+    model_config <- lst_config[["config_files"]][["GOTM-Selmaprotbas"]]
+    
+    path_parts <- strsplit(row_dict[1, "path"], "/")[[1]]
+    names(path_parts) <- paste0("key", 1:length(path_parts))
+    path_parts <- c(path_parts,
+                    "value" = value * row_dict[1, "conversion"],
+                    "file" = file.path(folder, model_config),
+                    "verbose" = verbose)
+    arglist = split(path_parts, names(path_parts)) # Turn into named list
+    do.call(input_yaml_multiple, args = arglist)
+  }
   
 }
